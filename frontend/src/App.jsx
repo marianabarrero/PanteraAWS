f9a83471c3e76fbe97f3f79898219998e5b7886a
Mariana <marianabarrero24@gmail.com>
2025-09-17 02:00:28 -0500

    New

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,7 +46,7 @@ const ErrorMessage = ({ error, onRetry }) => (
 );
 
 const LocationInfo = ({ location, formatTimestamp }) => (
-  <div className='glassmorphism-strong rounded-4xl max-w-[100%] p-8'>
+  <div className='max-w-[100%] p-8'>
     <h2 className='text-2xl font-bold text-white text-center rounded-4xl mb-8'>Última Ubicación</h2>
     
     <div className='flex flex-row justify-between gap-4 glassmorphism group rounded-xl mb-3 pl-2 pr-6 py-2'>
@@ -230,7 +230,7 @@ function App() {
         </div>
       </header>
 
-      <main className='flex flex-col md:flex-row items-center mt-32 md:mt-12 justify-between gap-2 max-w-[90%] mx-auto min-h-screen'>
+      <main className='flex flex-row md:flex-row items-center mt-32 md:mt-12 justify-between gap-2 max-w-[90%] mx-auto min-h-screen'>
         {loading ? (
           <LoadingSpinner />
         ) : error ? (
